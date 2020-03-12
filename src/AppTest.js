3a2cdbc8680bc31a120cb3de39a637177a87e600
nsbernstein52 <57200889+nsbernstein52@users.noreply.github.com>
2020-03-12 15:54:03 -0600

    Rename to preclude test

---
+++ src/AppTest.js
@@ -0,0 +1,11 @@
+// was App.test.js changed to AppTest.js nsb
+
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
