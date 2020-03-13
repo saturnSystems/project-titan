49343224b175cf98489275d11c24e25085466ed9
nsbernstein52 <57200889+nsbernstein52@users.noreply.github.com>
2020-03-13 15:45:43 -0600

    Move global layout to RIAC

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -5,6 +5,7 @@ import Col from 'react-bootstrap/Col';
 import Qa from './components/q-a/Q-a.jsx';
 import Overview from './components/overview/Overview';
 import Reviews from "./components/reviews/Reviews";
+import RIAC from "./components/RIAC/RIAC";
 
 function Layout(props) {
   return (
@@ -26,89 +27,7 @@ function Layout(props) {
       <Overview />
       <br></br>
 
-      <Container-fluid className="layout container">
-        <Col sm={{ span: 10, offset: 1 }} className="layout container">
-          <Row className="layout">Related Products</Row>
-          <Row className="layout">
-            <Col className="layout">
-              <Row className="layout">IMAGE</Row>
-              <Row className="layout">CATEGORY</Row>
-              <Row className="layout">
-                Expanded Product Name with Extra Text
-              </Row>
-              <Row className="layout">$123</Row>
-              <Row className="layout">*****</Row>
-            </Col>
-            <Col className="layout">
-              <Row className="layout">IMAGE</Row>
-              <Row className="layout">CATEGORY</Row>
-              <Row className="layout">
-                Expanded Product Name with Extra Text
-              </Row>
-              <Row className="layout">$123</Row>
-              <Row className="layout">*****</Row>
-            </Col>
-            <Col className="layout">
-              <Row className="layout">IMAGE</Row>
-              <Row className="layout">CATEGORY</Row>
-              <Row className="layout">
-                Expanded Product Name with Extra Text
-              </Row>
-              <Row className="layout">$123</Row>
-              <Row className="layout">*****</Row>
-            </Col>
-            <Col className="layout">
-              <Row className="layout">IMAGE</Row>
-              <Row className="layout">CATEGORY</Row>
-              <Row className="layout">
-                Expanded Product Name with Extra Text
-              </Row>
-              <Row className="layout">$123</Row>
-              <Row className="layout">*****</Row>
-            </Col>
-          </Row>
-          <br></br>
-          <Row className="layout">Your Outfit</Row>
-          <Row className="layout">
-            <Col className="layout">
-              <Row className="layout">IMAGE</Row>
-              <Row className="layout">CATEGORY</Row>
-              <Row className="layout">
-                Expanded Product Name with Extra Text
-              </Row>
-              <Row className="layout">$123</Row>
-              <Row className="layout">*****</Row>
-            </Col>
-            <Col className="layout">
-              <Row className="layout">IMAGE</Row>
-              <Row className="layout">CATEGORY</Row>
-              <Row className="layout">
-                Expanded Product Name with Extra Text
-              </Row>
-              <Row className="layout">$123</Row>
-              <Row className="layout">*****</Row>
-            </Col>
-            <Col className="layout">
-              <Row className="layout">IMAGE</Row>
-              <Row className="layout">CATEGORY</Row>
-              <Row className="layout">
-                Expanded Product Name with Extra Text
-              </Row>
-              <Row className="layout">$123</Row>
-              <Row className="layout">*****</Row>
-            </Col>
-            <Col className="layout">
-              <Row className="layout">IMAGE</Row>
-              <Row className="layout">CATEGORY</Row>
-              <Row className="layout">
-                Expanded Product Name with Extra Text
-              </Row>
-              <Row className="layout">$123</Row>
-              <Row className="layout">*****</Row>
-            </Col>
-          </Row>
-        </Col>
-      </Container-fluid>
+      <RIAC />
       <br></br>
 
       <Qa />
--- src/components/rIAC/RIAC.jsx
+++ src/components/rIAC/RIAC.jsx
@@ -3,10 +3,95 @@ import 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
-function RIAC(props){
-return(
+function RIAC(props) {
+  return (
 
-  
-)}
+    <Container-fluid className="layout container">
+    <Col sm={{ span: 10, offset: 1 }} className="layout container">
+      <Row className="layout">Related Products</Row>
+      <Row className="layout">
+        <Col className="layout">
+          <Row className="layout">IMAGE</Row>
+          <Row className="layout">CATEGORY</Row>
+          <Row className="layout">
+            Expanded Product Name with Extra Text
+          </Row>
+          <Row className="layout">$123</Row>
+          <Row className="layout">*****</Row>
+        </Col>
+        <Col className="layout">
+          <Row className="layout">IMAGE</Row>
+          <Row className="layout">CATEGORY</Row>
+          <Row className="layout">
+            Expanded Product Name with Extra Text
+          </Row>
+          <Row className="layout">$123</Row>
+          <Row className="layout">*****</Row>
+        </Col>
+        <Col className="layout">
+          <Row className="layout">IMAGE</Row>
+          <Row className="layout">CATEGORY</Row>
+          <Row className="layout">
+            Expanded Product Name with Extra Text
+          </Row>
+          <Row className="layout">$123</Row>
+          <Row className="layout">*****</Row>
+        </Col>
+        <Col className="layout">
+          <Row className="layout">IMAGE</Row>
+          <Row className="layout">CATEGORY</Row>
+          <Row className="layout">
+            Expanded Product Name with Extra Text
+          </Row>
+          <Row className="layout">$123</Row>
+          <Row className="layout">*****</Row>
+        </Col>
+      </Row>
+      <br></br>
+      <Row className="layout">Your Outfit</Row>
+      <Row className="layout">
+        <Col className="layout">
+          <Row className="layout">IMAGE</Row>
+          <Row className="layout">CATEGORY</Row>
+          <Row className="layout">
+            Expanded Product Name with Extra Text
+          </Row>
+          <Row className="layout">$123</Row>
+          <Row className="layout">*****</Row>
+        </Col>
+        <Col className="layout">
+          <Row className="layout">IMAGE</Row>
+          <Row className="layout">CATEGORY</Row>
+          <Row className="layout">
+            Expanded Product Name with Extra Text
+          </Row>
+          <Row className="layout">$123</Row>
+          <Row className="layout">*****</Row>
+        </Col>
+        <Col className="layout">
+          <Row className="layout">IMAGE</Row>
+          <Row className="layout">CATEGORY</Row>
+          <Row className="layout">
+            Expanded Product Name with Extra Text
+          </Row>
+          <Row className="layout">$123</Row>
+          <Row className="layout">*****</Row>
+        </Col>
+        <Col className="layout">
+          <Row className="layout">IMAGE</Row>
+          <Row className="layout">CATEGORY</Row>
+          <Row className="layout">
+            Expanded Product Name with Extra Text
+          </Row>
+          <Row className="layout">$123</Row>
+          <Row className="layout">*****</Row>
+        </Col>
+      </Row>
+    </Col>
+    </Container-fluid>
+    <br></br>
+
+  )
+}
 
 export default RIAC;
