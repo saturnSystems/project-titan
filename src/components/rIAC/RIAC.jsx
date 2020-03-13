af870c86bb65e1128e42fcbcacbf8ca61412cc0f
nsbernstein52 <57200889+nsbernstein52@users.noreply.github.com>
2020-03-13 15:43:48 -0600

    Create component

---
+++ src/components/rIAC/RIAC.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+function RIAC(props){
+return(
+
+  
+)}
+
+export default RIAC;
