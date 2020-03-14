7c035711ff2b267e8952e5c31c429187fc2d5124
Austin Abbott <magicaustin51547@gmail.com>
2020-03-14 13:00:18 -0600

    Dynamically render questions

--- src/components/q-a/Q-a-RenderList.jsx
+++ src/components/q-a/Q-a-RenderList.jsx
@@ -1,6 +1,7 @@
 // this component will go in Q-a.jsx
 import React from 'react';
-const dataFunctions = require('/Users/austin/Documents/HackReactor/project-titan/src/helper/helper.js');
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 // getListQuestions
 
 
@@ -9,10 +10,44 @@ class RenderList extends React.Component {
         super(props)
     }
 
+     // question.question_body
     render() {
-        const items = this.props.questions.map(question => <div>{ question.question_body }</div>)
+        const items = this.props.questions.map(question =>
+        <Row className="layout">
+        <Col className="layout">
+        <Row className="layout">Q:
+        <Col className="layout" sm={9}>
+        <Row className="layout">{question.question_body}</Row>
+        </Col>
+        <Col className="layout">
+        <Row className="layout">Helpful? | Add answer</Row>
+        </Col>
+        </Row>
+        <Row className="layout">A:
+        <Col className="layout" sm={9}>
+        <Row className="layout">
+        <Col className="layout">
+        <Row className="layout">Answer Text</Row>
+        <Row className="layout">{question.question_date} | Helpful? {'Yes (' + question.question_helpfulness + ')'} | Report</Row>
+        </Col>
+        </Row>
+        <Row className="layout">
+        <Col className="layout">
+        <Row className="layout">Answer to reply?</Row>
+        <Row className="layout">IMAGES</Row>
+        <Row className="layout">Date | Helpful | Report</Row>
+        </Col>
+        </Row>
+        </Col>
+        </Row>
+        <Row>
+        <Col>Load more answers</Col>
+        </Row>
+        </Col>
+        </Row>
+        )
         return (
-          <div>{ items }</div>
+          <div> { items } </div>
         )
     }
 }
--- src/components/q-a/Q-a.jsx
+++ src/components/q-a/Q-a.jsx
@@ -16,44 +16,12 @@ class Qa extends React.Component {
     console.log(this.props)
     return (
       <div>
-      <RenderList questions={this.props.questions}/>
       <Container-fluid className="layout container">
         <Col sm={{ span: 10, offset: 1 }} className="layout container">
         <Row className="layout">{"QUESTIONS & ANSWERS"}</Row>
         <Row className="layout">Search Bar</Row>
-        <Row className="layout">
-        <Col className="layout">
-        <Row className="layout">Q:
-        <Col className="layout" sm={9}>
-        <Row className="layout">Who What When</Row>
-        </Col>
-        <Col className="layout">
-        <Row className="layout">Helpful? | Add answer</Row>
-        </Col>
-        </Row>
-        <Row className="layout">A:
-        <Col className="layout" sm={9}>
-        <Row className="layout">
-        <Col className="layout">
-        <Row className="layout">Answer Text</Row>
-        <Row className="layout">Date | Helpful | Report</Row>
-        </Col>
-        </Row>
-        <Row className="layout">
-        <Col className="layout">
-        <Row className="layout">Answer to reply?</Row>
-        <Row className="layout">IMAGES</Row>
-        <Row className="layout">Date | Helpful | Report</Row>
-        </Col>
-        </Row>
-        </Col>
-        </Row>
-        <Row>
-        <Col>Load more answers</Col>
-        </Row>
-        </Col>
-        </Row>
-        <Row className="layout">
+        <RenderList questions={this.props.questions}/>
+        {/* <Row className="layout">
         <Col className="layout">
         <Row className="layout">Q:
         <Col className="layout" sm={9}>
@@ -83,7 +51,7 @@ class Qa extends React.Component {
         <Col>Load more answers</Col>
         </Row>
         </Col>
-        </Row>
+        </Row> */}
         <Row className="layout">MORE ANSWERED QUESTIONS | ADD A QUESTION +</Row>
         </Col>
         </Container-fluid>
