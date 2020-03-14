661072acac62b431af8d8632ceb57700a772d4f6
Gage <gagemw@gmail.com>
2020-03-13 18:30:13 -0600

    Remove extraneous import

--- src/components/overview/Overview.jsx
+++ src/components/overview/Overview.jsx
@@ -3,7 +3,6 @@ import 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import StarRatings from 'react-star-ratings'
-import Reviews from '../reviews/Reviews';
 
 class Overview extends React.Component{
 constructor(props){
