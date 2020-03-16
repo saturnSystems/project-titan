14926e45544286b576c726cdc50a1c05bc685494
Xi99 <32117342+Xi99@users.noreply.github.com>
2020-03-16 09:07:44 -0600

    Reviews (#36)
    * Clean up some code in helper and show number of helpfulness to user on each review
    
    * Give user the ability to cast a vote if they thought it was a helpful review and only allow them to click it once and only increment once
    
    * Change helpfulness number incrementing solution
    
    * Refactor to how eslint AirBnB likes it
    
    * Change defaultProps of review to {} instead of []

--- src/components/reviews/ReviewsTiles/ReviewTiles.jsx
+++ src/components/reviews/ReviewsTiles/ReviewTiles.jsx
@@ -1,22 +1,23 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from "react";
 import "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import "./ReviewsTiles.css";
 import StarRatings from "react-star-ratings";
+import PropTypes from "prop-types";
 
-export default class ReviewTiles extends React.Component {
+class ReviewTiles extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      reviews: [],
       helpfulness: 0,
       clicked: false
     };
   }
 
-  //////////// TODO ///////////////////////
+  // ////////// TODO ///////////////////////
   /**
  * Review Body - The review body will be a free-form multimedia input where the user can submit text and images regarding their experience with the product. 
                  The text submitted as part of the review will be between 50 and 1000 characters long.  
@@ -28,58 +29,64 @@ export default class ReviewTiles extends React.Component {
  * Rating Helpfulness - There is no data for "No" so only doing "Yes". Will add as future implementation
 
  */
+
   componentDidMount = () => {
-    this.setState({ helpfulness: this.props.review.helpfulness });
+    const { review } = this.props;
+    this.setState({ helpfulness: review.helpfulness });
   };
 
-  handleHelpfulness = () => {
-    // e.preventDefault();
-
-    this.setState({ helpfulness: ++this.state.helpfulness });
+  handleHelpfulness = ({ helper }) => {
+    const { review } = this.props;
+    const { helpfulness } = this.state;
+    this.setState({ helpfulness: helpfulness + 1 });
     this.setState({ clicked: true });
-    this.props.helper.putHelpfulReview(this.props.review.review_id, () => true);
+    helper.putHelpfulReview(review.review_id, () => true);
   };
 
   render() {
-    if (this.props.review.body.length < 50) {
+    const { review } = this.props;
+    const { helpfulness } = this.state;
+    const { clicked } = this.state;
+    const { date } = this.props;
+    if (review.body.length < 50) {
       return null;
     }
     return (
       <dl>
-        <React.Fragment key={this.props.review.review_id}>
+        <React.Fragment key={review.review_id}>
           <Row className="layout">
             <Col className="layout">
               <Row className="layout">
                 <Col className="layout" sm={2}>
                   <Row className="layout stars">
                     <StarRatings
-                      rating={this.props.review.rating}
+                      rating={review.rating}
                       starDimension="1em"
-                      starSpacing={"0"}
+                      starSpacing="0"
                     />{" "}
                   </Row>
                 </Col>
                 <Col className="layout" sm={{ offset: 3 }}>
                   <Row className="layout floaty">
-                    {this.props.review.reviewer_name}, {this.props.date}
+                    {review.reviewer_name}, {date}
                   </Row>
                 </Col>
               </Row>
               <Row className="layout">
                 <Col className="layout">
                   <Row className="layout">
-                    <strong>{this.props.review.summary.slice(0, 60)}</strong>
+                    <strong>{review.summary.slice(0, 60)}</strong>
                   </Row>
-                  {this.props.review.summary.length > 60 ? (
+                  {review.summary.length > 60 ? (
                     <Row className="layout">
-                      {"... " + this.props.review.summary.slice(60)}
+                      {`... ${review.summary.slice(60)}`}
                     </Row>
                   ) : null}
-                  {this.props.review.body.length <= 1000 ? (
-                    <Row className="layout">{this.props.review.body}</Row>
+                  {review.body.length <= 1000 ? (
+                    <Row className="layout">{review.body}</Row>
                   ) : null}
                   <Row className="layout">
-                    {this.props.review.recommend === 1 ? (
+                    {review.recommend === 1 ? (
                       <p>&#10004; I recommend this product</p>
                     ) : null}
                   </Row>
@@ -87,22 +94,22 @@ export default class ReviewTiles extends React.Component {
               </Row>
               <Row className="layout">
                 <Col className="layout response">
-                  {typeof this.props.review.response == "string" &&
-                  this.props.review.response !== "null" ? (
+                  {typeof review.response === "string" &&
+                  review.response !== "null" ? (
                     <dl>
                       <Row className="layout response">
                         Response From Seller:
                       </Row>
-                      <Row className="layout response">
-                        {this.props.review.response}
-                      </Row>
+                      <Row className="layout response">{review.response}</Row>
                     </dl>
                   ) : null}
                 </Col>
               </Row>
               <Row className="layout">
                 Helpful?{" "}
-                {this.state.clicked === false ? (
+                {clicked === false ? (
+                  // eslint-disable-next-line jsx-a11y/click-events-have-key-events
+                  // eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions
                   <p
                     name="helpfulness"
                     className="buffer"
@@ -114,7 +121,7 @@ export default class ReviewTiles extends React.Component {
                 ) : (
                   <p className="buffer">Yes</p>
                 )}
-                ({this.state.helpfulness}) | Report
+                ({helpfulness}) | Report
               </Row>
             </Col>
           </Row>
@@ -123,3 +130,15 @@ export default class ReviewTiles extends React.Component {
     );
   }
 }
+
+ReviewTiles.propTypes = {
+  review: PropTypes.objectOf(PropTypes.shape),
+  date: PropTypes.string
+};
+
+ReviewTiles.defaultProps = {
+  review: {},
+  date: ""
+};
+
+export default ReviewTiles;
