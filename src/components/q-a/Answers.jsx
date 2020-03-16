dd00c35a4970f0de9f31a2d836125e64a650a552
Austin Abbott <magicaustin51547@gmail.com>
2020-03-16 13:40:41 -0600

    Remove bootstrap Row and Col imports where they weren't needed

--- src/components/q-a/Answers.jsx
+++ src/components/q-a/Answers.jsx
@@ -17,34 +17,34 @@ class Answers extends React.Component {
   }
 
     render() {
-    const items = this.state.answers.map((answer, i) => {    
-    return (
-    <Row className="layout" key={i}>A:
+    const items = this.state.answers.map((answer, i) => {
+      return (
+        <Row className="layout" key={i}>
+        A:
 
-    <Col className="layout" sm={9}>
+          <Col className="layout" sm={9}>
       <Row className="layout">
         <Col className="layout">
           <Row className="layout">{ answer.body }</Row>
 
-              {/* <Row className="layout">{new Date(
-                  question.question_date
-               ).toLocaleDateString("en-US",
+          <Row className="layout">{new Date(answer.date).toLocaleDateString("en-US",
     {weekday: "long", 
     year: "numeric", 
     month: "short",  
-    day: "numeric"})} | Helpful? {' Yes (' + question.question_helpfulness + ')'} | Report</Row> */}
-    </Col>
-    </Row>
-    <Row className="layout">
+    day: "numeric"})} | Helpful? {' Yes (' + answer.helpfulness + ')'} | Report</Row>
+        </Col>
+      </Row>
+      <Row className="layout">
         <Col className="layout">
           <Row className="layout">Answer to reply?</Row>
-    <Row className="layout">IMAGES</Row>
-    <Row className="layout">Date | Helpful | Report</Row>
-    </Col>
-    </Row>
-    </Col>
-    </Row> 
-    )})
+          <Row className="layout">IMAGES</Row>
+          <Row className="layout">Date | Helpful | Report</Row>
+        </Col>
+      </Row>
+          </Col>
+        </Row>
+      );
+    });
 
     return (
         <div>{ items }</div>
--- src/components/q-a/List.jsx
+++ src/components/q-a/List.jsx
@@ -1,7 +1,7 @@
 // this component will go in Q-a.jsx
 import React from 'react';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
 import Questions from '../q-a/Questions';
 
 
