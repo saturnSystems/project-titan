32355780ab250d747a2bbd4c846c08a4f2176b2f
Austin Abbott <magicaustin51547@gmail.com>
2020-03-16 14:18:34 -0600

    Refactor code to abide by linter's indentation rules

--- src/components/q-a/Q-a.jsx
+++ src/components/q-a/Q-a.jsx
@@ -1,64 +1,25 @@
 // Q-a.jsx stub
-import React from "react";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import List from "./List";
+import React from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import List from './List';
 
-class Qa extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    // console.log(this.props)
-    return (
-      <div>
-      <Container-fluid className="layout container">
-          <Col sm={{ span: 10, offset: 1 }} className="layout container">
-            <Row className="layout">"QUESTIONS & ANSWERS"</Row>
-            <Row className="layout">Search Bar</Row>
-            <List questions={this.props.questions}/>
-        {/* <Row className="layout">
-        <Col className="layout">
-        <Row className="layout">Q:
-        <Col className="layout" sm={9}>
-        <Row className="layout">Who What When</Row>
-        </Col>
-        <Col className="layout">
-        <Row className="layout">Helpful? | Add answer</Row>
-        </Col>
-        </Row>
-        <Row className="layout">A:
-        <Col className="layout" sm={9}>
+const Qa = (props) => (
+  <div>
+    <Container-fluid className="layout container">
+      <Col sm={{ span: 10, offset: 1 }} className="layout container">
         <Row className="layout">
-        <Col className="layout">
-        <Row className="layout">Answer Text</Row>
-        </Col>
+        "QUESTIONS & ANSWERS"
         </Row>
+        <Row className="layout">Search Bar</Row>
+        <List questions={props.questions}/>
         <Row className="layout">
-        <Col className="layout">
-        <Row className="layout">Answer to reply?</Row>
-        <Row className="layout">IMAGES</Row>
-        <Row className="layout">Date | Helpful | Report</Row>
-        </Col>
+          MORE ANSWERED QUESTIONS | ADD A QUESTION +
         </Row>
-        </Col>
-        </Row>
-        <Row>
-        <Col>Load more answers</Col>
-        </Row>
-        </Col>
-        </Row> */}
-            <Row className="layout">
-              MORE ANSWERED QUESTIONS | ADD A QUESTION +
-            </Row>
-          </Col>
-        </Container-fluid>
-        <br />
-      </div>
-    )
-  }
-}
+      </Col>
+    </Container-fluid>
+    <br />
+  </div>
+);
 
 export default Qa;
--- src/components/q-a/Questions.jsx
+++ src/components/q-a/Questions.jsx
@@ -2,36 +2,39 @@
 import React from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Answers from "./Answers";
+import Answers from './Answers';
 
 
-class Questions extends React.Component {
+const Questions = (props) => {
 
-  render() {
-    const items = this.props.questions.map((question, i) =>
-      <Row className="layout" key={i}>
-        <Col className="layout">
-          <Row className="layout">Q:
-         <Col className="layout" sm={9}>
-          <Row className="layout">{question.question_body}</Row>
-            </Col>
-            <Col className="layout">
-              <Row className="layout">Helpful? | Add answer</Row>
-            </Col>
-          </Row>
-          <Answers questionId={question.question_id}/>
+  const items = props.questions.map((question, i) => (
+    <Row className="layout" key={i}>
+      <Col className="layout">
+        <Row className="layout">
+        Q:
+          <Col className="layout" sm={9}>
+            <Row className="layout">{question.question_body}</Row>
+          </Col>
+          <Col className="layout">
+            <Row className="layout">Helpful? | Add answer</Row>
+          </Col>
+        </Row>
+        <Answers questionId={question.question_id} />
         <Row>
-        <Col>Load more answers</Col>
+          <Col>Load more answers</Col>
         </Row>
-        </Col>
-        </Row>
-        )
-        return (
-          <div> { items } </div>
-        )
-    }
-}
+      </Col>
+    </Row>
 
+  ),
+  );
+
+  return (
+    <div>
+      { items }
+    </div>
+  );
+};
 
 
 export default Questions;
