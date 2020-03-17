c67903f594e92a8b022e54eb756581a32da7d094
Neil Bernstein <57200889+nsbernstein52@users.noreply.github.com>
2020-03-17 13:10:49 -0600

    Rp - Close to finishing TrelloCard #1 (#47)
    * Defer 1 col to ProductCard
    
    * Implement Card with static data
    
    * Make RP caption strong
    
    * Remove border from .module-header
    
    * Debug and format card
    
    * Clean up  compiler warnings
    
    * Add relatedProducts to state and arg of RIAC
    
    * Make RIAC render only if currentProduct exists
    
    * Pass relatedProductsId as prop to RIAC
    
    * Pass relatedProductId to ProductCard
    
    * Use props for all data, except styles
    
    * Pass style and ratings props to RIAC
    
    * Use props for style and rating
    
    * Get styles to use prop, temp: currentProd
    
    * Modify to relatedProducts
    
    * Change prop to relatedProductsIds
    
    * Attempt to use appropriate styles
    
    * Comment out console.logs
    
    * Add relatedProducts to state
    
    * Comment out an if around a console.log
    
    * Add setCurrentProduct and pass as prop to RIAC
    
    * Add setCurrentProduct as prop and attempt to update relatedProductId
    
    * setProductId instead of setProduct
    
    * Clean up extraneous code
    
    * Delete extraneous comments
    
    * Delete log in setProductId
    
    * Log productID for debugging, then comment out
    
    * Delete dead: code/logs/comments

--- src/components/rIAC/comp/ProductCard/ProductCard.jsx
+++ src/components/rIAC/comp/ProductCard/ProductCard.jsx
@@ -1,175 +1,88 @@
-// this component will go in RIAC.jsx
-//   for use in both Related Products and Your Outfit
+// this component is for use in RIAC.jsx
+//   for both Related Products and Your Outfit
 import React from 'react';
-// import Row from 'react-bootstrap/Row'; // not yet used
-// import Col from 'react-bootstrap/Col'; // not yet used
+
 const helper = require("../../../../helper/helper.js");
 
-// \project-titan\src\components\rIAC\comp\ProductCard\ProductCard.jsx
-// \project-titan\src\helper\helper.js
 class ProductCard extends React.Component {
   constructor(props) {
     super(props)
     this.state={
       relatedProduct: {}
+      // relatedProductId: props.relatedProductId // not yet needed
     }
-
   }
 
-  componentDidMount() {
-
-    if (this.props.relatedProductId !== undefined) {
-  // console.log("PC: cDM: t.p.rPId: ", this.props.relatedProductId)
-    }
-    // if (this.props.relatedProductId !== undefined) {
-      // helper.getOneProduct(this.props.relatedProductId, result => {
-        helper.getOneProduct(3, result => {
-          this.setState({
-            relatedProduct: result
-          });
-        });  
-    // }
-
-    // if (this.props.relatedProductId !== undefined) {
-      // helper.getOneProductStyle(this.props.relatedProductId, result=>{
+  componentDidUpdate(prevProps) {
+    let currentRelatedProductId = this.props.relatedProductId
+    if (typeof currentRelatedProductId === 'number' && currentRelatedProductId !== prevProps.relatedProductId) {
+    // console.log("PC: cDU: cRPId: ", currentRelatedProductId)
+      helper.getOneProduct(3, result => { // HARD CODED
+        this.setState({
+          relatedProduct: result
+        });
+      });  
       helper.getOneProductStyle(11, result=>{
         this.setState({
           styles: result.results
         })
       });
-    // }  
+   }
+
   }
 
-  // const currentImage = "https://images.unsplash.com/photo-1501088430049-71c79fa3283e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80";
-  // https://images.unsplash.com/photo-1501088430049-71c79fa3283e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80
-  // ProductCard body
+  setProductId = (event) => {
+    this.props.setProductId(this.props.relatedProductId);
+  }
+
+  scrollToTop () {
+    window.scrollTo(0, 0);
+  }
+
   render() {
-// console.log("PC: ENTER: t.p: ", this.props);
-
-    const currentProduct = this.props.currentProduct;
-// console.log("PC: cP: ", currentProduct);
-    // const relProductId = this.props.relatedProductId;
-    // console.log("PC: rP: ", relProductId);
-////
+    // const currentProduct = this.props.currentProduct; // not currently needed
+    // console.log("PC: cP: ", currentProduct);
     const relatedProductId = this.props.relatedProductId;
+    // console.log("PC: rPId: ", relatedProductId);
 
-//     if (this.props.relatedProductId !== undefined) {
-// console.log("PC: rPId: ", relatedProductId);
-//     }
     const relatedProduct = this.state.relatedProduct;
-// console.log("PC: t.s.rP: ", this.state.relatedProduct);
-// console.log("PC: rP: ", relatedProduct);
-    // if (relatedProduct !== undefined && relatedProduct[relatedProductId] !== undefined) {
-    //   console.log("PC: rP[rPId]: ", relatedProduct[relatedProductId] );
-    // }
-    // const relProduct = this.state.relatedProduct;
-    // if (relProduct[0] !== undefined) {
-    //   console.log("PC: rP: ", relProduct);
-    // }
     const relatedCategory = relatedProduct.category;
-    // console.log(relatedCategory);
     const relatedCaption = relatedProduct.name + ' - ' + relatedProduct.slogan;
-    // console.log(relatedCaption);
     const relatedDefPrice = relatedProduct.default_price;
-    // console.log(relatedDefPrice);
     const relatedRating = "***";
-    // console.log(relatedRating);
     
-    // if (this.props.styles[0] !== undefined) {
-    //   console.log("PC: t.p.s: ", this.props.styles)
-    // }
-    const relatedPropsStyles = this.props.styles;
-    if (relatedPropsStyles[0] !== undefined) {
-      // console.log("PC: rPsSs[0]: ", relatedPropsStyles[0])
-    }
-    if (this.state.styles !== undefined) {
-      // console.log("PC: t.s.s: ", this.state.styles)
-    }
-    const relatedStateStyles = this.state.styles;
-    if (relatedStateStyles !== undefined) {
-      // console.log("PC: rSS: ", relatedStateStyles)
-    }
-    const relatedPropStyleIndex = 0;
+    const relatedStyles = this.props.styles;
+      // console.log("PC: rPsSs[0]: ", relatedStyles[0])
 
-    const relatedPropStyle = this.props.styles[relatedPropStyleIndex];
-    if (relatedPropStyle !== undefined) {
-      // console.log("PC: rPS: ", relatedPropStyle);
+    const relatedStylesIndex = 1; // HARD CODED
+    const relatedStyle = relatedStyles[relatedStylesIndex];
+      // console.log("PC: rS: ", relatedStyle);
+  
+    let relThumbnail = "(NO IMAGE AVAILABLE)"; // fallback
+    const relatedThumbnailIndex = 0; // HARD CODED
+    if (this.props.styles[relatedStylesIndex] !== undefined) { // needed, but WHY?
+      relThumbnail = relatedStyle.photos[relatedThumbnailIndex].thumbnail_url;
     }
-    if (this.state.styles !== undefined) {
-      const relatedStateStyle = this.state.styles[relatedPropStyleIndex];
-// console.log("PC: rSS: ", relatedStateStyle);
-    }
-
-    const relThumbnailIndex = 0;
-    // const relThumbnail = relatedPropStyles[relatedPropStyleIndex].photos[relThumbnailIndex].thumbnail_url;
-    let relThumbnail = "(NO IMAGE AVAILABLE)";
-
-    if (this.props.styles[0] !== undefined) {
-      // relThumbnail = this.props.styles[0].photos[relThumbnailIndex].thumbnail_url;
-      relThumbnail = relatedPropsStyles[2].photos[relThumbnailIndex].thumbnail_url;
-    }
-
-    if (this.state.styles !== undefined && this.state.styles[0] !== undefined) {
-      // relThumbnail = this.state.styles[0].photos[relThumbnailIndex].thumbnail_url;
-      // relThumbnail = this.state.styles[0].photos[0].thumbnail_url;
-    }
-    // console.log("PC: rT: ", relThumbnail)
-    // const relCategory = relProduct.category;
-    // const relCaption = relProduct.name + ' - ' + relProduct.slogan;
-
-    // const relDefPrice = relProduct.default_price;
-
-    // const relRating = relProduct.TBD;
-
-    ////
-    // console.log("PC: reviewRating: ", this.props.reviewRating)
 
     return (
       <Container-fluid className="layout product-card-layout align-left">
-        <div id="product-card-div">
-          <a href="../../overview/Overview.jsx">
-          <div className="card mb-3">
-            <p>
-              {}
-              {/* <a className="card-link"> */}
-                {/* <img className="card-img-top" src="https://images.unsplash.com/photo-1501088430049-71c79fa3283e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80" alt="Card image cap" /> */}
-                {/* <img className="card-img-top" src="https://images.unsplash.com/photo-1501088430049-71c79fa3283e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80" alt="" /> */}
+        <div id="product-card-div" onClick={this.setProductId}>
+          <a onClick={this.scrollToTop}>
+            <div className="card mb-3">
+              <p>
                 <img className="card-img-top" src={relThumbnail}  alt=""/>
-                {/* <a href="../../overview/Overview.jsx"> <img className="card-img-top" src="https://images.unsplash.com/photo-1501088430049-71c79fa3283e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80" alt="Card image cap" />
-                </a> */}
-              {/* </a> */}
-            </p>
-            <div className="card-img-overlay">
-              {/* <p><a href="../../overview/Overview.jsx" className="btn btn-primary">X</a></p> */}
-              <small><p className="btn btn-primary">*</p></small>
+              </p>
             </div>
-          </div>
-        </a>
+          </a>
         </div>
         <div className="card">
           <div className="card-body">
-          {/* <p className="card-text category">Jackets</p> */}
-          {/* {const currCategory=this.props[0].category;} */}
           <p className="card-text category">{relatedCategory}</p>
-            {/* <h5 className="card-title caption">Camo Onesie - Forest Green &amp; Black </h5> */}
             <h5 className="card-title caption">{relatedCaption}</h5>
-            {/* <small><p className="card-text price" className="text-muted">$140</p></small> */}
             <small><p className="card-text text-muted price">{relatedDefPrice}</p></small>
             <p className="card-text">{relatedRating}</p>
           </div>
-          {/* <img class="card-img-bottom" src="..." alt="Card image cap"> */}
         </div>
-      {/* <Row className="layout">
-        <Col  sm={{ span: 4 }} className="layout container align-left">
-          <Row className="layout">IMAGE</Row>
-          <Row className="layout category">CATEGORY</Row>
-          <Row className="layout caption">
-            Expanded Product Name with Extra Text
-          </Row>
-          <Row className="layout price">$123</Row>
-          <Row className="layout stars">*****</Row>
-        </Col>
-      </Row> */}
       </Container-fluid>
     )
   }
