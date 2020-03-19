f8c67d27eccebbce01f35c869d9c059fc271f33b
Austin Abbott <57454313+AustinAbbott@users.noreply.github.com>
2020-03-18 19:40:52 -0600

    Q a search (#56)
    * Show Seller name in bold
    
    * Render only two answers on initial page load
    
    * Render two additional answers when button is clicked
    
    * Render only 4 questions on initial page load
    
    * Render MORE ANSWERED QUESTIONS button after set number of questions are rendered
    
    * Render more questions when button is clicked
    
    * Render questions in order of helpfulness
    
    * Clean-up code
    
    * Save work for review
    
    * Add search button to Questions module
    
    * Render questions with text that match the search term
    
    * Render filtered questions after search term is greater than 2 characters
    
    * Clean up code for final commit to search branch

--- src/components/q-a/Questions.jsx
+++ src/components/q-a/Questions.jsx
@@ -1,8 +1,10 @@
-// this component will go in Q-a.jsx
 import React from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import FormControl from 'react-bootstrap/FormControl';
 import Answers from './Answers';
+
 
 
 class Questions extends React.Component {
@@ -10,12 +12,20 @@ class Questions extends React.Component {
     super(props);
     this.state = {
       renderQuestions: 4,
+      SearchText: '',
     };
     this.ShowTwoMore = this.ShowTwoMore.bind(this);
+    this.SearchBox = this.SearchBox.bind(this);
   }
 
   ShowTwoMore() {
     this.setState((prevState) => ({ renderQuestions: prevState.renderQuestions + 2 }));
+  }
+
+  SearchBox(event) {
+    this.setState({
+      SearchText: event.target.value.toUpperCase(),
+    });
   }
 
   render() {
@@ -29,24 +39,33 @@ class Questions extends React.Component {
     }
 
     sortedQuestions = sortedQuestions.sort(compare).slice(0, this.state.renderQuestions);
+    if (this.state.SearchText.length >= 3) {
+      sortedQuestions = sortedQuestions.filter(question => question.question_body
+        .toUpperCase().includes(this.state.SearchText));
+    }
+
     const items = sortedQuestions.map((question, i) => (
-      <div key={i}>
-        <Row className="layout" >
+      <div>
+        <Row className="layout" key={i}>
           <Col className="layout">
             <Row className="layout">
-        Q:
+              <b>Q:&nbsp;</b>
               <Col className="layout" sm={9}>
-                <Row className="layout">{question.question_body}</Row>
+                <Row className="layout"><b>{question.question_body}</b></Row>
               </Col>
               <Col className="layout">
-                <Row className="layout">Helpful? {' Yes (' + question.question_helpfulness + ')'} | Add answer</Row>
+                <Row className="layout">
+                Helpful?
+                  {' Yes (' + question.question_helpfulness + ') '}
+                | Add answer
+                </Row>
               </Col>
             </Row>
             <Answers questionId={question.question_id} />
           </Col>
         </Row>
         {i === sortedQuestions.length - 1 && i !== this.props.questions.length - 1
-          ? <Row className="layout"><button onClick={this.ShowTwoMore}>MORE ANSWERED QUESTIONS</button> | ADD A QUESTION +</Row>
+          ? <Row className="layout"><Button onClick={this.ShowTwoMore}>MORE ANSWERED QUESTIONS</Button> | ADD A QUESTION +</Row>
           : null}
       </div>
 
@@ -54,6 +73,7 @@ class Questions extends React.Component {
 
     return (
       <div>
+        <Row className="layout"><div><form><FormControl type="text" placeholder="Have a question? Search for answers…" onChange={this.SearchBox} /></form></div></Row>
         { items }
       </div>
     );
