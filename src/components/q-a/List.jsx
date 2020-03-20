c9969e5154addc0691ab59a8654ba19a35368f28
Austin Abbott <57454313+AustinAbbott@users.noreply.github.com>
2020-03-20 08:50:12 -0600

    Q a helpfulness (#62)
    * Show Seller name in bold
    
    * Render only two answers on initial page load
    
    * Render two additional answers when button is clicked
    
    * Render only 4 questions on initial page load
    
    * Render MORE ANSWERED QUESTIONS button after set number of questions are rendered
    
    * Render more questions when button is clicked
    
    * Render questions in order of helpfulness
    
    * Clean-up code
    
    * Save work for review
    
    * Add search button to Questions module
    
    * Render questions with text that match the search term
    
    * Render filtered questions after search term is greater than 2 characters
    
    * Clean up code for final commit to search branch
    
    * Refactored some functions to ES6
    
    * Refactor Questions to give each question its own state
    
    * Add button functionality back after refactoring what props were passed between components
    
    * Render a collapse button when all answers are rendered
    
    * Add unique keys to each child element

--- src/components/q-a/List.jsx
+++ src/components/q-a/List.jsx
@@ -1,15 +1,57 @@
-// this component will go in Q-a.jsx
 import React from 'react';
-// import Row from 'react-bootstrap/Row';
-// import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
+import FormControl from 'react-bootstrap/FormControl';
 import Questions from '../q-a/Questions';
 
 
-const List = (props) => (
-  <div>
-    <Questions questions={props.questions} />
-  </div>
-);
+class List extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+			SearchText: '',
+			renderQuestions: 2,
+    };
+	}
+	
+	ShowTwoMore = e => {
+    this.setState((prevState) => ({ renderQuestions: prevState.renderQuestions + 2 }));
+	}
+	
+	SearchBox = event => {
+    this.setState({
+      SearchText: event.target.value.toUpperCase(),
+    });
+  }
+
+  render() {
+    let questions = this.props.questions;
+
+    function compare(a, b) {
+      if (a.question_helpfulness < b.question_helpfulness) {
+        return 1;
+      }
+      return -1;
+    }
+
+    questions = questions.sort(compare).slice(0, this.state.renderQuestions);
+    if (this.state.SearchText.length >= 3) {
+      questions = questions.filter(question => question.question_body
+        .toUpperCase().includes(this.state.SearchText));
+		}
+		
+		return (
+			<div key={this.props.product_id}>
+			<Row className="layout"><div><form><FormControl type="text" placeholder="Have a question? Search for answers…" onChange={this.SearchBox} /></form></div></Row>
+			{questions.map(question => <Questions OneQuestion={question} key={question.question_id}/>)}
+			{this.state.renderQuestions < this.props.questions.length && this.props.questions.length > 2
+			? <Row className="layout"><Button onClick={this.ShowTwoMore}>MORE ANSWERED QUESTIONS</Button> | ADD A QUESTION +</Row> 
+			: null}
+		</div>
+		)
+	}
+
+}
 
 
 export default List;
