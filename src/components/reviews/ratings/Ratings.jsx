8af2342e41f8c9db18b77aba3c1d501094d5caf8
Sean Lorman <sean.lorman@gmail.com>
2020-03-20 21:41:44 -0600

    Have characteristic ratings rendering dynamically for most productId's

--- src/components/reviews/ratings/Ratings.jsx
+++ src/components/reviews/ratings/Ratings.jsx
@@ -1,9 +1,10 @@
+/* eslint-disable react/no-unused-state */
 /* eslint-disable arrow-parens */
 /* eslint-disable no-return-assign */
 /* eslint-disable no-param-reassign */
 /* eslint-disable comma-dangle */
-/* eslint-disable react/no-unused-state */
 /* eslint-disable quotes */
+
 /* eslint-disable react/prop-types */
 /* eslint-disable react/destructuring-assignment */
 import React, { Component } from "react";
@@ -13,6 +14,8 @@ import Col from "react-bootstrap/Col";
 import "./Ratings.css";
 import StarRatings from "react-star-ratings";
 import ProgressBar from "react-bootstrap/ProgressBar";
+import "react-bootstrap-range-slider/dist/react-bootstrap-range-slider.css";
+import RangeSlider from "react-bootstrap-range-slider";
 
 const helper = require("../../../helper/helper.js");
 
@@ -26,12 +29,13 @@ export default class Ratings extends Component {
       ratingStars: [],
       ratingVals: [],
       ratingsValsSum: 0,
-      ratingSum: 0,
       ratingsAvg: 0,
-      reviews: [],
-      ratingsLength: 0,
-      starArr: ["1", "2", "3", "4", "5"],
-      theSum: 0
+      size: 0,
+      width: 0,
+      comfort: 0,
+      quality: 0,
+      Length: 0,
+      fit: 0
     };
   }
 
@@ -42,6 +46,28 @@ export default class Ratings extends Component {
       // eslint-disable-next-line implicit-arrow-linebreak
       this.setState({
         meta: results,
+        size: results.characteristics.Size // productId 11 has problem because it is null
+          ? parseFloat(results.characteristics.Size.value.slice(0, 3))
+          : -1,
+        width: results.characteristics.Width
+          ? parseFloat(results.characteristics.Width.value.slice(0, 3))
+          : -1,
+        comfort: results.characteristics.Comfort
+          ? parseFloat(results.characteristics.Comfort.value.slice(0, 3))
+          : -1,
+        quality: results.characteristics.Quality
+          ? parseFloat(results.characteristics.Quality.value.slice(0, 3))
+          : -1,
+        Length: results.characteristics.Length
+          ? parseFloat(results.characteristics.Length.value.slice(0, 3))
+          : -1,
+        fit: results.characteristics.Fit
+          ? parseFloat(results.characteristics.Fit.value.slice(0, 3))
+          : -1,
+        recommendedSum: Object.values(results.recommended).reduce(
+          (a, b) => a + b
+        ),
+        recommendedYes: Object.values(results.recommended)[1],
         ratingStars: Object.keys(results.ratings),
         ratingVals: Object.values(results.ratings),
         ratingsValsSum: Object.values(results.ratings).reduce((a, b) => a + b),
@@ -72,7 +98,6 @@ export default class Ratings extends Component {
     );
   }
 
-  // bring in handleStarSort somehow??????
   render() {
     const { handleStarSort } = this.props;
     return (
@@ -88,7 +113,13 @@ export default class Ratings extends Component {
           </span>
         </Row>
         <br />
-        <Row className="layout">100% of reviews recommend</Row>
+        <Row className="layout">
+          {(
+            (this.state.recommendedYes / this.state.recommendedSum) *
+            100
+          ).toFixed(0)}
+          % of reviews recommend
+        </Row>
         <Row className="layout">
           <Col sm={2}>
             <Row onClick={() => handleStarSort(5)} className="underline">
@@ -192,21 +223,143 @@ export default class Ratings extends Component {
           </Col>
         </Row>
         <br />
+        {this.state.size !== -1 ? (
+          <Row className="layout">
+            <Col className="noPaddy">
+              <Row className="noPaddy"><strong>Size:</strong></Row>
+              <RangeSlider
+                className="rangy"
+                tooltip="auto"
+                tooltipPlacement="top"
+                step={0.1}
+                onChange={() => true}
+                min={1}
+                max={5}
+                value={this.state.size}
+              />
+              <Row className="noPaddy smallerText">
+                <Col sm={4}>Too small</Col>
+                <Col sm={4}>Perfect</Col>
+                <Col sm={4}>Too Large</Col>
+              </Row>
+            </Col>
+          </Row>
+        ) : null}
+        <br />
+        {this.state.width !== -1 ? (
+          <Row className="layout">
+            <Col className="noPaddy">
+              <Row className="noPaddy"><strong>Width:</strong></Row>
+              <RangeSlider
+                className="rangy"
+                tooltip="auto"
+                tooltipPlacement="top"
+                step={0.1}
+                onChange={() => true}
+                min={1}
+                max={5}
+                value={this.state.width}
+              />
+              <Row className="noPaddy smallerText">
+                <Col sm={4}>Too small</Col>
+                <Col sm={4}>Perfect</Col>
+                <Col sm={4}>Too Large</Col>
+              </Row>
+            </Col>
+          </Row>
+        ) : null}
+        <br />
         <Row className="layout">
-          <Col>
-            <Row>Size</Row>
-            <Row>||||||||||||||||||</Row>
-            <Row>Too small | Perfect | Too Large</Row>
+          <Col className="noPaddy">
+            <Row className="noPaddy"><strong>Comfort:</strong></Row>
+            <RangeSlider
+              className="rangy"
+              tooltip="auto"
+              tooltipPlacement="top"
+              step={0.1}
+              onChange={() => true}
+              min={1}
+              max={5}
+              value={this.state.comfort}
+            />
+            <Row className="noPaddy smallerText">
+              <Col sm={4} style={{ float: "left" }}>
+                Poor
+              </Col>
+              <Col sm={4} />
+              <Col sm={4}>Perfect</Col>
+            </Row>
           </Col>
         </Row>
         <br />
+        {this.state.quality !== -1 ? (
+          <Row className="layout">
+            <Col className="noPaddy">
+              <Row className="noPaddy"><strong>Quality:</strong></Row>
+              <RangeSlider
+                className="rangy"
+                tooltip="auto"
+                tooltipPlacement="top"
+                step={0.1}
+                onChange={() => true}
+                min={1}
+                max={5}
+                value={this.state.quality}
+              />
+              <Row className="noPaddy smallerText">
+                <Col sm={4}>Poor</Col>
+                <Col sm={4} />
+                <Col sm={4}>Great</Col>
+              </Row>
+            </Col>
+          </Row>
+        ) : null}
+        {this.state.Length !== -1 ? (
+          <Row className="layout">
+            <Col className="noPaddy">
+              <Row className="noPaddy"><strong>Length:</strong></Row>
+              <RangeSlider
+                className="rangy"
+                tooltip="auto"
+                tooltipPlacement="top"
+                step={0.1}
+                onChange={() => true}
+                min={1}
+                max={5}
+                value={this.state.Length}
+              />
+              <Row className="noPaddy smallerText">
+                <Col sm={4}>Too small</Col>
+                <Col sm={4}>Perfect</Col>
+                <Col sm={4}>Too Large</Col>
+              </Row>
+            </Col>
+          </Row>
+        ) : null}
+        <br />
+        {this.state.fit !== -1 ? (
         <Row className="layout">
-          <Col>
-            <Row>Comfort</Row>
-            <Row>||||||||||||||||||</Row>
-            <Row>Poor | Perfect</Row>
+          <Col className="noPaddy">
+            <Row className="noPaddy"><strong>Fit:</strong></Row>
+            <RangeSlider
+              className="rangy"
+              tooltip="auto"
+              tooltipPlacement="top"
+              step={0.1}
+              onChange={() => true}
+              min={1}
+              max={5}
+              value={this.state.fit}
+            />
+            <Row className="noPaddy smallerText">
+              <Col sm={4}>Too small</Col>
+              <Col sm={4}>Perfect</Col>
+              <Col sm={4}>Too Large</Col>
+            </Row>
           </Col>
         </Row>
+        ) : null }
+        <br />
       </dl>
     );
   }
