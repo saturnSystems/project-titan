28e07674534d2f865d9c463045e9599742772b79
Austin Abbott <magicaustin51547@gmail.com>
2020-03-25 14:53:18 -0600

    Add upload button to answer modal

--- src/components/q-a/AddAnswer.jsx
+++ src/components/q-a/AddAnswer.jsx
@@ -6,6 +6,7 @@ import ModalBody from 'react-bootstrap/ModalBody';
 import ModalFooter from 'react-bootstrap/ModalFooter';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
 const helper = require('../../helper/helper.js');
 
 
@@ -16,6 +17,7 @@ class AddAnswer extends React.Component {
       Answer: '',
       Nickname: '',
       Email: '',
+      Photos: [],
       ErrorMessage: '',
       AnswerError: '',
       NicknameError: '',
@@ -33,6 +35,13 @@ class AddAnswer extends React.Component {
 
   Email = (e) => {
     this.setState({Email: e.target.value});
+  }
+
+  Photos = (e) => {
+    console.log(e.target.value)
+    let morePhotos = this.state.Photos;
+    morePhotos.push(e.target.value);
+    this.setState({Photos: morePhotos}, () => console.log(this.state.Photos));
   }
 
   CheckForErrors = () => {
@@ -69,8 +78,8 @@ class AddAnswer extends React.Component {
     return (
       <Modal size="lg" show={true}>
         <ModalHeader><h2>Submit your Answer</h2></ModalHeader>
-          <ModalTitle>{this.props.currentProduct.name}: {this.props.questionBody}</ModalTitle>
             <ModalBody>
+            <ModalTitle>{this.props.currentProduct.name}: {this.props.questionBody}</ModalTitle>
               <Form>
               <p style={{color: 'red'}}><i>{this.state.ErrorMessage}</i></p>
                 <Form.Label><b>Your Answer*</b></Form.Label>
@@ -83,6 +92,8 @@ class AddAnswer extends React.Component {
                 <Form.Label><b>Your email*</b></Form.Label>
                 <Form.Control maxLength={"60"} type={`email`} placeholder={`Example: jack@email.com`} onChange={this.Email}></Form.Control>
                 <p style={{color: 'red'}}><i>{this.state.EmailError}</i></p>
+                <Form.Label><b>Upload Pictures (5 max.)</b></Form.Label>
+                <div><input type="file" onChange={this.Photos} /></div>
               </Form>
             </ModalBody>
         <ModalFooter>
