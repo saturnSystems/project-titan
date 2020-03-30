70456ccc073d1640c63c1293f4d00b14b9b15e7e
Austin Abbott <57454313+AustinAbbott@users.noreply.github.com>
2020-03-29 18:52:59 -0600

    Q a fine tune (#102)
    * Add CSS to prevent padding from being added to highlighted text
    
    * Close the answer modal after an answer is successfully submitted
    
    * Add URL inputs to AddAnswer modal
    
    * Final commit for branch. Add's photo URL's to state

--- src/components/q-a/List.jsx
+++ src/components/q-a/List.jsx
@@ -31,15 +31,7 @@ class List extends React.Component {
   };
 
   render() {
-    //   var instance = new Mark(document.querySelector("div.Question"));
-    //   instance.mark(this.state.SearchText, { accuracy: {
-    //     value: "exactly",
-    //     limiters: [".", ",", "!"]
-    //   },
-    // done: function(counter){
-    //   instance.unmark()
-    // }});
-
+    
     let questions = this.props.questions;
 
     function compare(a, b) {
@@ -72,7 +64,7 @@ class List extends React.Component {
             type="text"
             placeholder="Have a question? Search for answers…"
             onChange={this.SearchBox}
-            id="Search"
+            id={this.state.SearchText}
           />
         </Row>
         {questions.map(question => (
